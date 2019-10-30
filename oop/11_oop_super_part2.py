60e498a0c306734ad7ca6b8f9b79f08acd5687fd
wi fon <wifon@localhost.localdomain>
2019-10-30 20:26:04 +0100

    super() - part 2 example

---
+++ oop/11_oop_super_part2.py
@@ -0,0 +1,66 @@
+class User:
+
+    active_users = 0
+
+    @classmethod
+    def display_active_users(cls):
+        return "there are currently {} active users".format(cls.active_users)
+
+    @classmethod
+    def from_string(cls, data_str):
+        first, last, age = data_str.split(",")
+        return cls(first, last, int(age))
+
+    def __init__(self, first, last, age):
+        self.first = first
+        self.last = last
+        self.age = age
+        User.active_users += 1
+
+    def __repr__(self):
+        return "{} is {}".format(self.first, self.age)
+
+    def logout(self):
+        User.active_users -= 1
+        return "{} has logged out".format(self.first)
+
+    def full_name(self):
+        return "{} {}".format(self.first, self.last)
+
+    def initials(self):
+        return "{}.{}.".format(self.first[0], self.last[0])
+
+    def eats(self, item):
+        return "{} eats {}".format(self.first, item)
+
+    def is_senior(self):
+        return self.age >= 65
+
+    def birthday(self):
+        self.age += 1
+        return "Happy {}th birthday {}".format(self.age, self.first)
+
+
+class Moderator(User):
+    total_mods = 0
+    def __init__(self, first, last, age, community):
+        super().__init__(first, last, age)
+        self.community = community
+        Moderator.total_mods += 1
+
+    @classmethod
+    def display_active_mods(cls):
+        return "There are currently {} active mods".format(Moderator.total_mods)
+
+    def remove_post(self):
+        return "{} remove post from {} community".format(self.full_name(), self.community)
+
+print(User.display_active_users())
+user1 = User("Phil", "Goodman", 72)
+print(User.display_active_users())
+user2 = User("Gru", "Sia", 18)
+print(User.display_active_users())
+print(Moderator.display_active_mods())
+mod1 = Moderator("Wi", "Fon", 42, "news.com")
+print(User.display_active_users())
+print(Moderator.display_active_mods())
