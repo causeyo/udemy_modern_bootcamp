d54f6934e1780bd774a66a7b356272c1a7b9f1d5
causeyo <wifonik@gmail.com>
2019-11-06 22:18:32 +0100

    generator function - excercise

--- oop/21_oop_generators_summing.py
+++ oop/21_oop_generators_summing.py
@@ -10,13 +10,25 @@ list(default_multiples) # [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 '''
 
 def get_multiples(num=1, count=10):
-    counter = 1
+    counter = 0
+    number = num
     while counter < count:
-        yield num
-        num += num
+        yield number
+        number += num
         counter += 1
 
-default_multiples = get_multiples()
+default_multiples = get_multiples(2,3)
 
 for i in default_multiples:
     print(i)
+
+"""
+solution from course
+
+def get_multiples(num=1, count=10):
+    next_num = num
+    while count > 0:
+        yield next_num
+        count -= 1
+        next_num += num
+"""
