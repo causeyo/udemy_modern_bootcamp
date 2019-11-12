127a8787967a490fcec7e5ef46ba2f33b446c971
causeyo <wifonik@gmail.com>
2019-11-12 17:59:51 +0100

    pokerdeck - own unittests example

---
+++ testing_with_python/05_unittest_pokerdeck/pokerdeck_tests.py
@@ -0,0 +1,11 @@
+"""
+Your goal in this assignment will be to add tests to the classes you created in the last section: Card  and Deck .
+Try to test that instances have the right structure, and write some tests for the methods.
+Use some before hooks, and try to test for expected errors as well!
+
+Note that the shuffle  method will be difficult to test, since it produces a random output.
+Rather than trying to test randomness, you may just want to write some smaller, more straightforward tests.
+For instance, you could test that shuffle  doesn't change the size of the deck,
+or that the list of cards before the shuffle is in a different order than after the shuffle.
+
+"""
