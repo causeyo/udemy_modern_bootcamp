6bd8a0e347f516170647f13a924a43fd069911fe
causeyo <wifonik@gmail.com>
2019-11-20 16:27:27 +0100

    bs - scraping and writing to csv example

--- beautiful_soup/04_bs_extract.py
+++ beautiful_soup/04_bs_extract.py
@@ -6,9 +6,18 @@ import csv
 web_page = "https://www.rithmschool.com/blog"
 
 response = requests.get(web_page)
+# print(response.text) # whole html -> we don't want it
+
 soup = BeautifulSoup(response.text, "html.parser")
-# print(soup)
-data = soup.find_all("tr")
-# data = soup.find_all(class_="border_right_bottom")
-for elem in data:
-    print(elem)
+articles = soup.find_all("article")
+
+with open("blog_data.csv", "w") as csv_file:
+    csv_writer = writer(csv_file)
+    csv_writer.writerow(["title", "link", "date"])
+
+    for article in articles:
+        a_tag = article.find("a")
+        title = a_tag.get_text()
+        url = a_tag['href']
+        date = article.find("time")["datetime"]
+        csv_writer.writerow([title, url, date])
