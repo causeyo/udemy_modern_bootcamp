71a67d8c4e691d4e5a966400c0fa0a1450e8d91f
causeyo <wifonik@gmail.com>
2019-11-29 11:23:05 +0100

    excercises - part 02

--- excercise_challenge/02_excercise.py
+++ excercise_challenge/02_excercise.py
@@ -1,5 +1,5 @@
 def titleize(sentence):
-    return " ".join(word[0].upper()+word[1:] for word in sentence.split())
+    return " ".join(word[0].upper() + word[1:] for word in sentence.split())
 
 
 """
@@ -9,7 +9,7 @@ titleize('oNLy cAPITALIZe fIRSt') # "ONLy CAPITALIZe FIRSt"
 
 
 def find_factors(number):
-    return [elem for elem in range(1, number//2+1) if number % elem == 0].append(number)
+    return [elem for elem in range(1, number+1) if number % elem == 0]
 
 
 """
@@ -20,5 +20,54 @@ find_factors(321421) # [1,293,1097,321421 ]
 find_factors(412146) # [1,2,3,6,7,9,14,18,21,42,63,126,3271,6542,9813,19626,22897,29439,45794,58878,68691,137382,206073,412146]
 """
 
-a = find_factors(10)
-print(a)
+
+def includes(collection, element, index=0):
+    if type(collection) != dict:
+        return element in collection[index:]
+    else:
+        return element in collection.values()
+
+
+"""
+includes([1, 2, 3], 1) # True 
+includes([1, 2, 3], 1, 2) # False 
+includes({ 'a': 1, 'b': 2 }, 1) # True 
+includes({ 'a': 1, 'b': 2 }, 'a') # False
+includes('abcd', 'b') # True
+includes('abcd', 'e') # False
+"""
+
+
+def repeat(sign, number):
+    return sign * number
+
+
+"""
+repeat('*', 3) # '***' 
+repeat('abc', 2) # 'abcabc' 
+repeat('abc', 0) # ''
+"""
+
+
+def truncate(word, number):
+    if number<3:
+        return "Truncation must be at least 3 characters."
+    else:
+        if len(word) < number:
+            return word
+        return word[:number-3] + "..."
+
+
+"""
+truncate("Super cool", 2) # "Truncation must be at least 3 characters."
+truncate("Super cool", 1) # "Truncation must be at least 3 characters."
+truncate("Super cool", 0) # "Truncation must be at least 3 characters."
+truncate("Hello World", 6) # "Hel..."
+truncate("Problem solving is the best!", 10) # "Problem..."
+truncate("Another test", 12) # "Another t..."
+truncate("Woah", 4) # "W..."
+truncate("Woah", 3) # "..."
+truncate("Yo",100) # "Yo"
+truncate("Holy guacamole!", 152) # "Holy guacamole!"
+"""
+
