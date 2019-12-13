01bcc2e11fe57b2356c2fdd90bb15a0fc0774a2c
causeyo <wifonik@gmail.com>
2019-12-13 12:48:03 +0100

    excercise - part 6

--- excercise_challenge/06_excercise.py
+++ excercise_challenge/06_excercise.py
@@ -46,14 +46,14 @@ primes = next_prime()
 # [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
 """
 
+
 def next_prime():
-    i = 1
+    i = 2
     while True:
-        if i > 1:
+        if i == 2:
             yield i
-
-
+        else:
+            if i%2:
+                if not [num for num in range(2, i//2) if not i % num]:
+                    yield i
         i += 1
-
-primes = next_prime()
-print([next(primes) for i in range(25)])
